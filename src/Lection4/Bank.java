1d5221cb02b49bd1ea07d0439ffefc30b804a69d
ChumakAndrey <chumakandrey7@gmail.com>
2016-09-09 21:25:40 +0300

    HomeTask #4

---
+++ src/Lection4/Bank.java
@@ -0,0 +1,70 @@
+package Lection4;
+
+/**
+ * Created by Администратор on 09.09.2016.
+ */
+public abstract class Bank {
+    private long id;
+    private  String bankCountry;
+    Currency currency;
+    private int numberOfEmployee;
+    private double avrSalaryOfEmployee;
+    private long rating;
+    private long totalCapital;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBankCountry() {
+        return bankCountry;
+    }
+
+    public void setBankCountry(String bankCountry) {
+        this.bankCountry = bankCountry;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public int getNumberOfEmployee() {
+        return numberOfEmployee;
+    }
+
+    public void setNumberOfEmployee(int numberOfEmployee) {
+        this.numberOfEmployee = numberOfEmployee;
+    }
+
+    public double getAvrSalaryOfEmployee() {
+        return avrSalaryOfEmployee;
+    }
+
+    public void setAvrSalaryOfEmployee(double avrSalaryOfEmployee) {
+        this.avrSalaryOfEmployee = avrSalaryOfEmployee;
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+    public void setRating(long rating) {
+        this.rating = rating;
+    }
+
+    public long getTotalCapital() {
+        return totalCapital;
+    }
+
+    public void setTotalCapital(long totalCapital) {
+        this.totalCapital = totalCapital;
+    }
+}
---
+++ src/Lection4/USBank.java
@@ -0,0 +1,7 @@
+package Lection4;
+
+/**
+ * Created by Администратор on 09.09.2016.
+ */
+public class USBank extends Bank {
+}
