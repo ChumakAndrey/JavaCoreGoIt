5d332d07832d52761d2dcd3ff4e5121a23fd35a9
ChumakAndrey <chumakandrey7@gmail.com>
2016-09-11 11:16:09 +0300

    Question in USBank

--- src/Lection4/Currency.java
+++ src/Lection4/Currency.java
@@ -4,6 +4,5 @@ package Lection4;
  * Created by Администратор on 09.09.2016.
  */
 public enum Currency {
-    USD, EUR
-
-}
+    USD, EUR;
+    }
--- src/Lection4/USBank.java
+++ src/Lection4/USBank.java
@@ -4,4 +4,57 @@ package Lection4;
  * Created by Администратор on 09.09.2016.
  */
 public class USBank extends Bank {
+    int summ;
+    Currency currency;
+    public USBank(int summ, Currency currency) {
+        this.summ = summ;
+        this.currency = currency;
+    }
+
+    @Override
+    int getLimitOfWithdrawal() {
+        switch (currency){
+            case USD:
+             return 1000;
+            case  EUR:
+                return 1200;
+        }
+    }
+
+    @Override
+    int getLimitOfFunding() {
+        switch (currency){
+            case USD:
+                return 0;
+            case  EUR:
+                return 10000;
+        }
+    }
+
+    @Override
+    int getMonthRate() {
+        switch (currency){
+            case USD:
+                return summ/100;
+            case  EUR:
+                return summ*2/100;
+        }
+
+    }
+
+    @Override
+    int getCommision(int summ) {
+        switch (currency){
+            case USD:
+                if (summ<1000){
+                    return summ*5/100;
+                }
+                else return summ*7/100;
+
+            case  EUR:
+                if (summ<1000)
+                    return summ*6/100;
+                else return summ*8/100;
+        }
+    }
 }
