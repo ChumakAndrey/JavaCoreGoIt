4dfef83849bae6be4f3883c572e1e004f6f05be0
AndrewChumak <chumakandrey7@gmail.com>
2016-11-06 12:22:15 +0200

    FinalProjectGroup#5

--- src/FinalProgectGroup5/Controller.java
+++ src/FinalProgectGroup5/Controller.java
@@ -1,7 +1,13 @@
 package FinalProgectGroup5;
 
+import java.util.Set;
+
 /**
  * Created by Администратор on 30.10.2016.
  */
 public class Controller {
+    Set<Hotel> findRoom (){
+
+       // return HashSet<Hotel>;
+    };
 }
---
+++ src/FinalProgectGroup5/HotelDAO.java
@@ -0,0 +1,18 @@
+package FinalProgectGroup5;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Администратор on 06.11.2016.
+ */
+public class HotelDAO {
+    public Set<Hotel> getRooms(String city, String hotelName, int price, int persons){
+        Set<Hotel> resultHotelList = new HashSet<>();
+
+
+
+
+
+    }
+}
